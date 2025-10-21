3b76756b20392f8424ea1c16d06ddfdea9e13481
Salvatore251086 <salvatoredamichelekoln@gmail.com>
2025-10-21 18:09:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rls-cache-v4'
+const CACHE_NAME = 'rls-cache-v7'
 const OFFLINE_URL = 'offline.html'
 
 const ASSETS = [
@@ -9,45 +9,47 @@ const ASSETS = [
   'offline.html',
   'assets/icons/icon-192.png',
   'assets/icons/icon-512.png',
+  'assets/icons/icon-512-maskable.png',
+  'assets/screenshots/home-wide-1920x1080.png',
+  'assets/screenshots/home-narrow-1080x1920.png',
   'favicon.ico'
 ]
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)))
+self.addEventListener('install', e => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE_NAME)
+    await Promise.all(ASSETS.map(async u => {
+      try { await c.add(u) } catch(_) { /* skip mancante */ }
+    }))
+  })())
   self.skipWaiting()
 })
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null))))
-  )
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => k !== CACHE_NAME ? caches.delete(k) : null))))
   self.clients.claim()
 })
 
-self.addEventListener('fetch', event => {
-  const req = event.request
-  if (req.mode === 'navigate') {
-    event.respondWith(
-      fetch(req)
-        .then(res => {
-          const copy = res.clone()
-          caches.open(CACHE_NAME).then(cache => cache.put(req, copy))
-          return res
-        })
-        .catch(() => caches.match(req).then(r => r || caches.match(OFFLINE_URL)))
+self.addEventListener('fetch', e => {
+  const r = e.request
+  if (r.mode === 'navigate') {
+    e.respondWith(
+      fetch(r).then(res => {
+        const copy = res.clone()
+        caches.open(CACHE_NAME).then(c => c.put(r, copy))
+        return res
+      }).catch(() => caches.match(r).then(x => x || caches.match(OFFLINE_URL)))
     )
     return
   }
-  event.respondWith(
-    caches.match(req).then(cached => {
-      const fetchAndCache = fetch(req)
-        .then(res => {
-          const copy = res.clone()
-          caches.open(CACHE_NAME).then(cache => cache.put(req, copy))
-          return res
-        })
-        .catch(() => cached)
-      return cached || fetchAndCache
+  e.respondWith(
+    caches.match(r).then(cached => {
+      const f = fetch(r).then(res => {
+        const copy = res.clone()
+        caches.open(CACHE_NAME).then(c => c.put(r, copy))
+        return res
+      }).catch(() => cached)
+      return cached || f
     })
   )
 })
