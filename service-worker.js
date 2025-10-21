da18231d15403cd4c8725eca34d79bc9e89cf00d
Salvatore251086 <salvatoredamichelekoln@gmail.com>
2025-10-21 18:23:07 +0200

    Upgrade service worker cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rls-cache-v7'
+const CACHE_NAME = 'rls-cache-v9'
 const OFFLINE_URL = 'offline.html'
 
 const ASSETS = [
@@ -7,49 +7,61 @@ const ASSETS = [
   'app.js',
   'manifest.webmanifest',
   'offline.html',
+  'favicon.ico',
   'assets/icons/icon-192.png',
   'assets/icons/icon-512.png',
   'assets/icons/icon-512-maskable.png',
   'assets/screenshots/home-wide-1920x1080.png',
-  'assets/screenshots/home-narrow-1080x1920.png',
-  'favicon.ico'
+  'assets/screenshots/home-narrow-1080x1920.png'
 ]
 
 self.addEventListener('install', e => {
   e.waitUntil((async () => {
     const c = await caches.open(CACHE_NAME)
     await Promise.all(ASSETS.map(async u => {
-      try { await c.add(u) } catch(_) { /* skip mancante */ }
+      try { await c.add(u) } catch (_) {}
     }))
   })())
   self.skipWaiting()
 })
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => k !== CACHE_NAME ? caches.delete(k) : null))))
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  )
   self.clients.claim()
 })
 
 self.addEventListener('fetch', e => {
   const r = e.request
+
   if (r.mode === 'navigate') {
     e.respondWith(
-      fetch(r).then(res => {
-        const copy = res.clone()
-        caches.open(CACHE_NAME).then(c => c.put(r, copy))
-        return res
-      }).catch(() => caches.match(r).then(x => x || caches.match(OFFLINE_URL)))
+      fetch(r)
+        .then(res => {
+          const copy = res.clone()
+          caches.open(CACHE_NAME).then(c => c.put(r, copy))
+          return res
+        })
+        .catch(() =>
+          caches.match(r).then(x => x || caches.match(OFFLINE_URL))
+        )
     )
     return
   }
+
   e.respondWith(
     caches.match(r).then(cached => {
-      const f = fetch(r).then(res => {
-        const copy = res.clone()
-        caches.open(CACHE_NAME).then(c => c.put(r, copy))
-        return res
-      }).catch(() => cached)
-      return cached || f
+      const net = fetch(r)
+        .then(res => {
+          const copy = res.clone()
+          caches.open(CACHE_NAME).then(c => c.put(r, copy))
+          return res
+        })
+        .catch(() => cached)
+      return cached || net
     })
   )
 })
